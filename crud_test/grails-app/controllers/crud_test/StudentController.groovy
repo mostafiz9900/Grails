36dc11e785735713d3fed33840b9bff603cc0af2
mostafiz9900 <mostafiz9900@gmail.com>
2019-08-31 00:02:21 +0600

    test crud

---
+++ crud_test/grails-app/controllers/crud_test/StudentController.groovy
@@ -0,0 +1,11 @@
+package crud_test
+
+class StudentController {
+
+    def index() {
+        render(view: "/studdent/index")
+    }
+    def remove(){
+        render("grails is best framwork in java laungruage")
+    }
+}
